160a2b756fdabe072995b1780710e190232c0baa
sachinsapsap <sachinsapsap@gmail.com>
2024-07-14 13:32:42 +0530

    First commit

---
+++ src/Test/HomePageTest.java
@@ -0,0 +1,9 @@
+package Test;
+
+public class HomePageTest {
+	
+	public void test1() {
+		System.out.println("test 1-HP");
+	}
+
+}
---
+++ src/Test/LoginPageTest.java
@@ -0,0 +1,9 @@
+package Test;
+
+public class LoginPageTest {
+	
+	public void test1() {
+		System.out.println("test 1-LP");
+	}
+
+}
