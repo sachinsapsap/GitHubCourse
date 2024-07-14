44faada59b35e6f41ec419f9ed47dfbab80797b2
sachinsapsap <sachinsapsap@gmail.com>
2024-07-14 14:21:31 +0530

    Added/updated/deleted files

--- src/Pages/LoginPage.java
+++ src/Pages/LoginPage.java
@@ -5,6 +5,8 @@ public class LoginPage {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello LP");
+		
+		System.out.println("Logged in");
 	}
 
 }
--- src/Test/HomePageTest.java
+++
@@ -1,9 +0,0 @@
-package Test;
-
-public class HomePageTest {
-	
-	public void test1() {
-		System.out.println("test 1-HP");
-	}
-
-}
