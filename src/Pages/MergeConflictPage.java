a23e91a827a06c2084950380d692525da8374199
sachinsapsap <sachinsapsap@gmail.com>
2024-07-17 09:37:36 +0530

    Resolved conflicts

--- src/Pages/MergeConflictPage.java
+++ src/Pages/MergeConflictPage.java
@@ -3,8 +3,10 @@ package Pages;
 public class MergeConflictPage {
 
 	public static void main(String[] args) {
-		
-		System.out.println("Remote changes");
+	
+
+		System.out.println("COnflicts resolved");
+
 
 	}
 
