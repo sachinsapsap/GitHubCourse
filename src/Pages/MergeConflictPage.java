01189fa5b03258901fbfc2d07617471caac76cf8
sachinsapsap <89699066+sachinsapsap@users.noreply.github.com>
2024-07-17 09:46:04 +0530

    Update MergeConflictPage.java

--- src/Pages/MergeConflictPage.java
+++ src/Pages/MergeConflictPage.java
@@ -5,7 +5,7 @@ public class MergeConflictPage {
 	public static void main(String[] args) {
 	
 
-		System.out.println("Conflicts resolved finally");
+		System.out.println("Conflicts resolved WHY");
 
 
 
